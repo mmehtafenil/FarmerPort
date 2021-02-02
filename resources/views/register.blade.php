1122095d160ea6fc0e0abae898863a4cf72f9e1c
Fenil Mehta <fenilmehts14@gmail.com>
2021-02-02 21:54:56 +0530

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,27 +5,28 @@
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
             <form action="register" method="POST">
+                @csrf
               <div class="form-group">
                 <label for="exampleInputEmail1">Name</label>
                 <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Full Name">
               </div>
               <div class="form-group">
                   <label for="exampleInputEmail1">Email Address</label>
-                  <input type="contact" name="contact" class="form-control" id="exampleInputEmail1" aria-describedby="emailContact" placeholder="Enter Email ID">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailContact" placeholder="Enter Email ID">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Address</label>
-                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Address">
+                  <input type="text" name="address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Address">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
                   <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                     <label class="radio-inline">
-                    <input type="radio" name="optradio" checked>Farmer
+                    <input type="radio" name="farmer">Farmer
                     </label>
                     <label class="radio-inline">
-                    <input type="radio" name="optradio" checked>Buyer
+                    <input type="radio" name="buyer">Buyer
                     </label>
                     <h5></h5>
                 <button type="submit" class="btn btn-primary" style="align-items:center">Register</button>
