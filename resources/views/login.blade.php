2d2d29545935bca40b853af0d9047e0a91bce3f8
Fenil Mehta <fenilmehts14@gmail.com>
2021-02-02 21:04:53 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,23 +1,21 @@
 @extends('home')
 @section('content')
 
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <div class="caption">
-        <h3>Admin</h3>
-        <p><a href="/adminlogin" class="btn btn-primary" role="button">Login</a></p>
-      </div>
-      <div class="caption">
-        <h3>Farmer</h3>
-        <p><a href="/farmerlogin" class="btn btn-primary" role="button">Login</a><a href="/farmerregister" class="btn btn-default" role="button">Register</a></p>
-      </div>
-      <div class="caption">
-        <h3>Buyer</h3>
-        <p><a href="/buyerlogin" class="btn btn-primary" role="button">Login</a><a href="/buyerregister" class="btn btn-default" role="button">Register</a></p>
-      </div>
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form action="login" method="POST">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+        </div>
     </div>
-  </div>
 </div>
-
 @endsection
